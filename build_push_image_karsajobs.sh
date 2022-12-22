60a13fb9e21a77eb6411535402e5ac1c9b884ddc
Arfan Hidayat Priyantono <arfan.hidayat.priyantono@gmail.com>
2022-12-22 18:24:35 +0700

    Update build_push_image_karsajobs.sh

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -4,6 +4,6 @@ docker build . -t yuukinaesa/karsajobs:latest
 # membuat tag baru dari karsajobs:latest ke github yuukinaesa ghcr.io/yuukinaesa/karsajobs:latest
 docker tag yuukinaesa/karsajobs:latest ghcr.io/yuukinaesa/karsajobs:latest
 # login ke Github Package
-echo dckr_pat_vbtBpTCNzlUZ6YPrCEwp7IFcyOw | docker login ghcr.io --username yuukinaesa --password-stdin
+echo $CR_PAT | docker login ghcr.io --username yuukinaesa --password-stdin
 # push ke Github Package  
 docker push ghcr.io/yuukinaesa/karsajobs:latest
