45a4f1c88258d3b8ccfcf35033ad4bad5da3b3e0
yuukinaesa <arfan470@gmail.com>
2022-12-22 20:53:24 +0700

    add tag

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Build image dengan nama yuukinaesa/karsajobs:latest
 docker build . -t yuukinaesa/karsajobs:latest
+# membuat tag baru dari karsajobs-ui:latest ke github yuukinaesa ghcr.io/yuukinaesa/karsajobs-ui:latest
+docker tag yuukinaesa/karsajobs-ui:latest ghcr.io/yuukinaesa/karsajobs-ui:latest
 # login ke Github Package
 echo $CR_PAT | docker login ghcr.io --username yuukinaesa --password-stdin
 # push ke Github Package  
